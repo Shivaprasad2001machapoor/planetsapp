4fef0e40e02216f50964d6cb63ad625c3982290d
shivaprasad <shivaprasadnomula75@gmail.com>
2023-10-13 04:31:42 +0000

    solution

--- src/components/PlanetItem/index.js
+++ src/components/PlanetItem/index.js
@@ -4,8 +4,8 @@ const PlanetItem = ({planetDetails}) => {
   const {name, imageUrl, description} = planetDetails
 
   return (
-    <div>
-      <img src={imageUrl} alt={name} className="pic" />
+    <div className="container" data-testid="planets">
+      <img src={imageUrl} alt={`planet ${name}`} className="pic" />
       <h1 className="name">{name}</h1>
       <p className="desc">{description}</p>
     </div>
@@ -13,3 +13,20 @@ const PlanetItem = ({planetDetails}) => {
 }
 
 export default PlanetItem
+
+/* import './index.css'
+
+const PlanetItem = ({planetDetails}) => {
+  const {name, imageUrl, description} = planetDetails
+
+  return (
+    <div className="container">
+      <img src={imageUrl} alt={`planet ${name}`} className="pic" />
+      <h1 className="name">{name}</h1>
+      <p className="desc">{description}</p>
+    </div>
+  )
+}
+
+export default PlanetItem
+*/
--- src/components/PlanetsSlider/index.js
+++ src/components/PlanetsSlider/index.js
@@ -12,6 +12,34 @@ const PlanetsSlider = ({planetsList}) => {
   }
 
   return (
+    <div>
+      <h1 data-testid="main-heading" className="main-heading">
+        PLANETS
+      </h1>
+      <Slider {...settings}>
+        {planetsList.map(planet => (
+          <PlanetItem key={planet.id} planetDetails={planet} />
+        ))}
+      </Slider>
+    </div>
+  )
+}
+
+export default PlanetsSlider
+/* import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import './index.css'
+import PlanetItem from '../PlanetItem'
+
+const PlanetsSlider = ({planetsList}) => {
+  const settings = {
+    dots: false,
+    slidesToScroll: 1,
+    slidesToShow: 1,
+  }
+
+  return (
     <div className="slider-container">
       <h1 className="main-heading">PLANETS</h1>
       <Slider {...settings}>
@@ -24,3 +52,4 @@ const PlanetsSlider = ({planetsList}) => {
 }
 
 export default PlanetsSlider
+*/
